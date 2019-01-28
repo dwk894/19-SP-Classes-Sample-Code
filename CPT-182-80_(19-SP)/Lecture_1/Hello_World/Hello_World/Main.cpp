4ab9cf6c7f33b61410c32ee3d607a28e23d2497d
Dayu Wang <dwang@STCHAS.EDU>
2019-01-28 14:42:01 -0600

    "Hello World" Program

---
+++ CPT-182-80_(19-SP)/Lecture_1/Hello_World/Hello_World/Main.cpp
@@ -0,0 +1,13 @@
+/** Created by Dayu Wang (dwang@stchas.edu) on 01-28-19. */
+
+/** Last updated by Dayu Wang (dwang@stchas.edu) on 01-28-19. */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+	system("pause");
+	return 0;
+}

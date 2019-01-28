78e466d14dd5cb527787aab46a710711085768f0
Dayu Wang <dwang@STCHAS.EDU>
2019-01-28 17:44:06 -0600

    Change Tabs to Spaces

--- CPT-182-80_(19-SP)/Lecture_1/Calculator/Calculator/Four-Function_Calculator.cpp
+++ CPT-182-80_(19-SP)/Lecture_1/Calculator/Calculator/Four-Function_Calculator.cpp
@@ -7,30 +7,30 @@
 using namespace std;
 
 int main() {
-	char again = 'Y';
-	while (again == 'Y') {
-		system("cls");
-		int num1, num2;
-		char op;
-		cout << "Enter the first number: ";
-		cin >> num1;
-		cout << "Enter the second number: ";
-		cin >> num2;
-		cout << "Enter the operation (case sensitive)." << endl;
-		cout << "A - Addition" << endl;
-		cout << "S - Subtraction" << endl;
-		cout << "M - Multiplication" << endl;
-		cout << "D - Division: ";
-		cin >> op;
-		if (op == 'A') { cout << num1 << " + " << num2 << " = " << num1 + num2 << endl; }
-		if (op == 'S') { cout << num1 << " - " << num2 << " = " << num1 - num2 << endl; }
-		if (op == 'M') { cout << num1 << " * " << num2 << " = " << num1 * num2 << endl; }
-		if (op == 'D') {
-			if (!num2) { cout << "Denominator is zero.  Cannot calculate." << endl; }
-			else { cout << num1 << " / " << num2 << " = " << num1 * 1.0 / num2 << endl; }
-		}
-		cout << "Do you want to do another calculation (Y/N)? ";
-		cin >> again;
-	}
-	return 0;
+    char again = 'Y';
+    while (again == 'Y') {
+        system("cls");
+        int num1, num2;
+        char op;
+        cout << "Enter the first number: ";
+        cin >> num1;
+        cout << "Enter the second number: ";
+        cin >> num2;
+        cout << "Enter the operation (case sensitive)." << endl;
+        cout << "A - Addition" << endl;
+        cout << "S - Subtraction" << endl;
+        cout << "M - Multiplication" << endl;
+        cout << "D - Division: ";
+        cin >> op;
+        if (op == 'A') { cout << num1 << " + " << num2 << " = " << num1 + num2 << endl; }
+        if (op == 'S') { cout << num1 << " - " << num2 << " = " << num1 - num2 << endl; }
+        if (op == 'M') { cout << num1 << " * " << num2 << " = " << num1 * num2 << endl; }
+        if (op == 'D') {
+            if (!num2) { cout << "Denominator is zero.  Cannot calculate." << endl; }
+            else { cout << num1 << " / " << num2 << " = " << num1 * 1.0 / num2 << endl; }
+        }
+        cout << "Do you want to do another calculation (Y/N)? ";
+        cin >> again;
+    }
+    return 0;
 }
